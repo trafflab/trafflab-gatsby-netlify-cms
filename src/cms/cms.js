5f323d1e5beb3daca84695b899ac571b1af9c3b7
Alan Ibatullin <mr.juneiak@gmail.com>
2022-07-10 03:48:19 +0300

    del product

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -4,7 +4,6 @@ import cloudinary from 'netlify-cms-media-library-cloudinary'
 
 
 import BlogPostPreview from './preview-templates/BlogPostPreview'
-import ProductPagePreview from './preview-templates/ProductPagePreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
 import ArticlePagePreview from './preview-templates/article-page-preview'
 CMS.registerMediaLibrary(uploadcare)
@@ -12,6 +11,5 @@ CMS.registerMediaLibrary(cloudinary)
 
 CMS.registerPreviewTemplate('index', IndexPagePreview)
 
-CMS.registerPreviewTemplate('products', ProductPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
 CMS.registerPreviewTemplate('articles', ArticlePagePreview)
