d34761e646c24aad174fb351adfe563204be1a7d
Alan Ibatullin <mr.juneiak@gmail.com>
2022-08-02 00:44:23 +0300

    test module

--- src/components/popups/popup-layout/popup-layout.jsx
+++ src/components/popups/popup-layout/popup-layout.jsx
@@ -27,7 +27,8 @@ export default function PopupLayout({children, isOpen, closeHandler}) {
       <section
         onClick={overlayClose}
         className={styles.popupLayout}
-        style={animation}
+        style={{...animation, display: shouldMount ? 'flex' : 'none'}}
+
       >
         {children}
       </section>
