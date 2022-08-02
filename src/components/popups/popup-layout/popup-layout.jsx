3a50ff126b4247c8f616cd8e90533ad60d9bbc4f
Alan Ibatullin <mr.juneiak@gmail.com>
2022-08-02 20:15:00 +0300

    test 2

--- src/components/popups/popup-layout/popup-layout.jsx
+++ src/components/popups/popup-layout/popup-layout.jsx
@@ -4,8 +4,8 @@ import useDelayUnmountState from '../../../hooks/use-delay-unmount-state';
 
 export default function PopupLayout({children, isOpen, closeHandler}) {
   
-  const shouldMount = useDelayUnmountState(!!isOpen, 350);
-  const animation = isOpen ? {animation: 'openAniamtion 0.35s ease-out'} : {animation: 'closeAniamtion 0.35s ease-out forwards'}
+  const shouldMount = useDelayUnmountState(!!isOpen, 1000);
+  const animation = isOpen ? {animation: 'openAniamtion 1s ease-out'} : {animation: 'closeAniamtion 1s ease-out forwards'}
 
   const overlayClose = (evt) => {
     if (evt.target.classList.contains(styles.popupLayout)) closeHandler()
