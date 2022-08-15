6db639d9d3196724e41c7ff44d6674a280f5bb50
Alan Ibatullin <mr.juneiak@gmail.com>
2022-08-15 21:29:19 +0300

    add blog redirect

---
+++ src/pages/ru/blog/index.jsx
@@ -0,0 +1,53 @@
+import React, { useEffect } from "react";
+import { navigate } from "gatsby";
+import Loader from "../../../components/common/loader/loader";
+
+const getRedirectLanguage = () => {
+
+  if (typeof navigator === `undefined`) {
+    return "en";
+  }
+
+  const lang = navigator && navigator.language && navigator.language.split("-")[0];
+  if (!lang) return "en";
+
+  switch (lang) {
+    case "en":
+      return "en";
+    case "ru":
+      return "ru";
+    case "uk":
+      return "ua";
+    default:
+      return "en";
+  }
+};
+
+export default function IndexPage() {
+
+  useEffect(() => {
+    let urlLang
+    const savedLang = localStorage.getItem('lang')
+    if (savedLang) urlLang = localStorage.getItem('lang')
+    else {
+      urlLang = getRedirectLanguage();
+      localStorage.setItem('lang', urlLang);
+    }
+    
+    navigate(`/${urlLang}#blog`, {replace: true})
+
+  }, []);
+
+  return (
+      <div style={{
+        height: '100vh',
+        widows: '100%',
+        backgroundColor: 'var(--color-dark-2)',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        }}>
+        <Loader />
+      </div>
+  ) 
+};
