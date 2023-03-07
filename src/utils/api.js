d1dd1fc68c07e488c5780296a0b8fb3f62452737
Alan Ibatullin <mr.juneiak@gmail.com>
2023-03-07 20:35:50 +0500

    hotfix

--- src/utils/api.js
+++ src/utils/api.js
@@ -6,7 +6,7 @@ export const sendFormToTg = (product, name, contact, comfyContact) => {
     Удобный канал связи - ${comfyContact}
   `
 
-  return fetch(`https://api.telegram.org/bot5502333932:AAFTPXDUZcEJ9c07zoJrZZDZVFZO9GUk0Eo/sendMessage?chat_id=-738135275&parse_mode=html&text=${message}`, {
+  return fetch(`https://api.telegram.org/bot5543193450:AAEAf0Z8sasjKkwn4VgSLk1CSM9IhpMgMQE/sendMessage?chat_id=-906397071&parse_mode=html&text=${message}`, {
     method: 'GET'
   })
   .then((res) => res.json())
